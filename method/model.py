3f68366348ad4135d24157e049c8d453c179dbfc
celaira <lauled@gmx.de>
2024-07-07 04:42:40 +0200

    .

--- method/model.py
+++ method/model.py
@@ -1,1 +1,15 @@
-#deep_learning
+import json
+import os
+import cv2 as cv
+import numpy as np
+from PIL import Image
+from google.colab.patches import cv2_imshow
+import tensorflow as tf
+from google.colab import drive
+drive.mount('/content/drive')
+from google.colab import files
+image = files.upload()
+label = files.upload()
+
+from google.colab import drive
+drive.mount('/content/drive')
